da72dacfbdb07542d2aa7106fd3a5d3277851dff
Sobuto <48230942+Sobuto@users.noreply.github.com>
2019-04-11 01:05:19 -0500

    Add UART.v IO

--- HDMI_Passthrough.srcs/sources_1/new/UART.v
+++ HDMI_Passthrough.srcs/sources_1/new/UART.v
@@ -21,6 +21,20 @@
 
 
 module UART(
-
+    input wire i_clk,
+    input wire i_tx_data,
+    input wire [7:0] i_tx_byte,
+    input wire o_tx_en,
+    input wire o_tx_serial,
+    input wire o_tx_done
     );
+    
+    
+parameter baud_rate = 9600;
+parameter data_bits = 8;
+parameter parity = 0; //0 = no parity bit, 1 = parity bit
+parameter stop_bit = 1;
+
+
+   
 endmodule
