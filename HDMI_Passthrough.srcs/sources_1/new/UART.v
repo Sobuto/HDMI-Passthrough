1c2c62a10bc4b76c52de1d0558805a3f72a5576b
Sobuto <48230942+Sobuto@users.noreply.github.com>
2019-04-10 22:59:24 -0500

    Add a UART.v

---
+++ HDMI_Passthrough.srcs/sources_1/new/UART.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 04/10/2019 10:37:28 PM
+// Design Name: 
+// Module Name: UART
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module UART(
+
+    );
+endmodule
